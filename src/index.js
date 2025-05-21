636d19dec6724fab69b58c35741b4d76a311a4fd
angelina <angelina.s.jacklin@gmail.com>
2025-05-21 20:16:11 +0200

    svarat på alla frågor i uppgiften

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ Create a variable that stores the name of your cafe.
 Print out a greeting such as "Welcome to Technigo Cafe! What would you like to order today?"
 Where Technigo Cafe is replaced by the name of your cafe that is stored in the variable.
 */
-const cafeName = "Technigo Cafe";
+const cafeName = "Angelina's Cafe";
 console.log(`Welcome to ${cafeName}! What would you like to order today?`);
 
 /*
@@ -15,12 +15,22 @@ Print out the total price such as "There you go, that'll be 10 euros"
 Where 10 is replaced by the calculation of the total price.
 */
 
+const coffePrice = 2
+let amountOfCoffes = 4
+console.log(`There you go, that'll be ${coffePrice * amountOfCoffes} euros`);
+
 /*
 3)
 Create a variable that stores a boolean.
 Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
+
+let costumerFeedback = true;
+
+if (costumerFeedback) {
+  console.log(`You said this coffee is the best coffe ever, that was actually ${costumerFeedback}`)
+}
 
 /*
 4)
@@ -31,6 +41,11 @@ Assign it a new value.
 Print it out. => This should give you the new value.
 */
 
+let cafeGuests = 14
+console.log(`We currently have ${cafeGuests} cafe guests`);
+cafeGuests = 20
+console.log(`We currently have ${cafeGuests} cafe guests`);
+
 /*
 5)
 Create a variable called maxGuests, that shows us how many guests are allowed in the cafe.
@@ -40,16 +55,28 @@ assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
 
+const maxGuests = 50
+console.log(`We currently have ${maxGuests} cafe guests which is our maximum amount`);
+maxGuests = 60
+
 /*
 6)
 Create a variable that stores a string.
 Print out that string in only UPPERCASE letters.
 */
 
+let someText = "Hello world";
+let uppercase = someText.toUpperCase();
+console.log(uppercase);
+
 /*
 7)
 Print out the same string in only lowercase letters.
 */
+
+let someText = "Hello world";
+let lowercase = someText.toLowerCase();
+console.log(lowercase);
 
 /*
 8) **BONUS**
@@ -58,3 +85,8 @@ Then figure out a way to replace the word "summer" in the string with the word "
 Should give you => "Today we have a special winter deal!"
 (check for a specific string method...)
 */
+
+let text = "Today we have a special summer deal!";
+let result = text.replace("summer", "winter");
+console.log(result)
+
